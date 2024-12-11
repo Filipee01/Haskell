75e25b717e1c69a7e5767b7582ca2bb2e5b407ff
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-12-11 13:31:19 -0300

    Update exec2.hs

--- exec2.hs
+++ exec2.hs
@@ -1,6 +1,6 @@
 import Prelude hiding (sum)
 
-sum :: [Int] -> Int --recebe uma lista de inteiros e retorna um inteiro
+sum :: [Int] -> Int --recebe uma lista de inteiros e retorna um inteiro, se a lista contém apenas um elemento [x], a soma é igual ao próprio elemento.
 sum [x] = x
 
 -- vai retorar o unico elemento presente na lista 
